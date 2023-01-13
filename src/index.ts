d9674cbc0d0fbe27b3d33b31e5b2269741853f20
Adhiana <adhianamastur48@gmail.com>
2023-01-13 15:43:28 +0700

    build(testing): setup package & testing

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+const color: Color = {
+  red: 10,
+  green: 20,
+  blue: 22,
+};
+
+console.log(color);
+
+export default color;
